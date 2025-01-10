152d3c7e0988aeec42588b10c209e50a1cac0e7e
Michaelgde <m66992524@gmail.com>
2025-01-10 09:29:44 -1100

    Update script.js

--- plugins/fnf key mapper/script.js
+++ plugins/fnf key mapper/script.js
@@ -2,17 +2,21 @@
 
 if(keyPressed.getAsString() === 'Left'){
    createNode(96)
+   runtimeScene.getObject('music_container')[runtimeScene.getObjects('music_container').length-1].getVariable().get('data').setString('left')
    keyPressed.setString('')
 }
 if(keyPressed.getAsString() === 'Down'){
    createNode(192)
+   runtimeScene.getObject('music_container')[runtimeScene.getObjects('music_container').length-1].getVariable().get('data').setString('down')
    keyPressed.setString('')
 }
 if(keyPressed.getAsString() === 'Up'){
    createNode(288)
+   runtimeScene.getObject('music_container')[runtimeScene.getObjects('music_container').length-1].getVariable().get('data').setString('up')
    keyPressed.setString('')
 }
 if(keyPressed.getAsString() === 'Right'){
    createNode(384)
+   runtimeScene.getObject('music_container')[runtimeScene.getObjects('music_container').length-1].getVariable().get('data').setString('right')
    keyPressed.setString('')
 }
