af393d3204e2323022b9a46bf45fa1db904a4ba9
Michaelgde <m66992524@gmail.com>
2025-01-10 10:26:06 -1100

    Update script.js

--- plugins/fnf key mapper/script.js
+++ plugins/fnf key mapper/script.js
@@ -3,20 +3,20 @@
 if(keyPressed.getAsString() === 'Left'){
    createNode(96)
    keyPressed.setString('')
-   runtimeScene.getObjects('music_container')[runtimeScene.getObjects('music_container').length-1].getVariable().get('data').setString('left')
+   runtimeScene.getObjects('music_container')[runtimeScene.getObjects('music_container').length-1].getVariables().get('data').setString('left')
 }
 if(keyPressed.getAsString() === 'Down'){
    createNode(192)
    keyPressed.setString('')
-   runtimeScene.getObjects('music_container')[runtimeScene.getObjects('music_container').length-1].getVariable().get('data').setString('down')
+   runtimeScene.getObjects('music_container')[runtimeScene.getObjects('music_container').length-1].getVariables().get('data').setString('down')
 }
 if(keyPressed.getAsString() === 'Up'){
    createNode(288)
    keyPressed.setString('')
-   runtimeScene.getObjects('music_container')[runtimeScene.getObjects('music_container').length-1].getVariable().get('data').setString('up')
+   runtimeScene.getObjects('music_container')[runtimeScene.getObjects('music_container').length-1].getVariables().get('data').setString('up')
 }
 if(keyPressed.getAsString() === 'Right'){
    createNode(384)
    keyPressed.setString('')
-   runtimeScene.getObjects('music_container')[runtimeScene.getObjects('music_container').length-1].getVariable().get('data').setString('right')
+   runtimeScene.getObjects('music_container')[runtimeScene.getObjects('music_container').length-1].getVariables().get('data').setString('right')
 }
