c0bcadfbf265e7757d2240af157bb2f76c715556
Michaelgde <m66992524@gmail.com>
2025-04-15 01:44:45 -1100

    Update script.js

--- plugins/see events/script.js
+++ plugins/see events/script.js
@@ -1,6 +1,6 @@
 if(runtimeScene.getOnceTriggers().triggerOnce(1)){
     registerTxt('event_display',true,false,30)
-    runtimeScene.createObject('event_display').setPosition(415,34)
+    runtimeScene.createObject('event_display').setPosition(415,14)
     runtimeScene.getObjects('event_display')[0].setLayer('Layer')
 }
 runtimeScene.getObjects('music_container').forEach(obj=>{if(gdjs.RuntimeObject.collisionTest(obj,runtimeScene.getObjects('play_node')[0],true)){
