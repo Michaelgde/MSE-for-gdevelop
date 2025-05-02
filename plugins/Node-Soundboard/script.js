895a7c3d99e521847b18d4fbf6711a86fc0e9d55
Byson94 <171326211+Byson94@users.noreply.github.com>
2025-05-02 15:20:15 +0530

    Update script.js
    useless line

--- plugins/Node-Soundboard/script.js
+++ plugins/Node-Soundboard/script.js
@@ -1,5 +1,3 @@
-// window.customNodeColors = window.customNodeColors || {}; oops
-
 document.addEventListener("keydown", function (event) {
   if (event.ctrlKey && event.shiftKey && event.key.toLowerCase() === "b") {
     if (!document.getElementById("cstmNodesoundboardIfame")) {
